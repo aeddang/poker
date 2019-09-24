28789e15be543402ed4deca92b25809e21339a91
Aeddang <aeddang2@gmail.com>
2019-09-24 02:20:09 -0700

    update

--- src/js/api/config.js
+++ src/js/api/config.js
@@ -1,6 +1,6 @@
 
 export const API_KEY = 'ironright';
-export const API_PATH = "http://localhost:10010/api/";
+export const API_PATH = "http://103.109.101.212:10010/api/";
 export const API_QUERY = "?api_key=" + API_KEY;
 
 export const ErrorCode = Object.freeze ({
