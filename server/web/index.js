ec9486ce925ef7b94d827eeef04401df6897b753
JeongCheol Kim <jeongcheolkim@homet-dev-app-ricky.local>
2019-09-25 18:37:53 +0900

    update

--- server/web/index.js
+++ server/web/index.js
@@ -1,9 +1,14 @@
 import express from 'express';
 import articles from './router/articles';
-
+import cors from  "cors";
 const port = Number(process.env.PORT || 2083);
 const app = express();
+var corsOptions = {
+  origin: "*",
+  methods: "GET,HEAD,PUT,PATCH,POST,DELETE"
+}
 
+app.use(cors(corsOptions));
 app.use('/', express.static(__dirname + '/../dist'));
 app.use('/articles', articles);
 
