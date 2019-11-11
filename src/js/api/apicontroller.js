fc58263f9731d0be47f3484f6536e0198a4408b7
JeongCheol Kim <jeongcheolkim@homet-dev-app-ricky.local>
2019-11-11 11:34:56 +0900

    update

--- src/js/api/apicontroller.js
+++ src/js/api/apicontroller.js
@@ -21,7 +21,7 @@ export function getUser(userId, rid = null){
   console.log(addedQuery)
   return Axios.request({
     method: 'get',
-    url: Config.API_PATH + 'users/' + userId + Config.API_QUERY + encodeURIComponent(addedQuery)
+    url: encodeURIComponent(Config.API_PATH + 'users/' + userId + Config.API_QUERY + addedQuery)
   });
 }
 
